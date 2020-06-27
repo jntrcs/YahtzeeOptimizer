1eded8ff5b3aa66ff7a394f1d0bb74bb7cfbd2bd
jntrcs <jcurtis210@gmail.com>
2020-06-27 17:56:22 -0600

    Build the turn graph

---
+++ BuildGraph.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 15:35:03 2020
+
+@author: jntrcs
+"""
+
+from GameState import *
+
+####Generate all possible vertices from one game card to another
+
+
+
+
+def findVertices(state, vertices):
+    vertices[state] = []
+    top = state.get_top()
+    for i in range(6):
+        if not top[i]:
+            for score in range(6):
+                vertices[state].append(state.mark_top(i+1, (score)*(i+1)))
+    
+    bottom = state.get_bottom()
+    for i in range(7):
+        if not bottom[i]:
+            vertices[state].append(state.mark_bottom(i))
+            if i ==6:
+               vertices[state].append(state.mark_bottom(i, True))
+    return vertices
+
+
+
+
+def check_self_and_all_children(state, vertices, checked):
+    if state in checked:
+        return vertices, checked
+    vertices = findVertices(state, vertices)
+    checked[state]=True
+    for i in vertices[state]:
+        vertices, checked = check_self_and_all_children(i, vertices, checked)
+    return vertices, checked
+
+
+new_game =  GameState()
+
+
+vertices = {}
+checked = {}
+        
+vertices, checked = check_self_and_all_children(new_game, vertices, checked)
+
+
---
+++ GameState.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 14:51:24 2020
+
+@author: jntrcs
+"""
+
+import copy
+
+class GameState:
+    def __init__(self):
+        self.top=[False]*6
+        self.top_score = 0
+        self.bottom = [False]*7
+        self.yahtzee = False
+        
+
+        
+    def get_top(self):
+        return self.top;
+    
+    def get_top_score(self):
+        return self.top_score;
+    
+    def get_bottom(self):
+        return self.bottom
+    
+    def get_yahtzee(self):
+        return self.yahtzee;
+    
+    def __hash__(self):
+        return hash(str(self))
+    
+    def __str__(self):
+        return "Top: " + str(self.top)+"\nTop Score: "+str(self.top_score)+"\nBottom: " + str(self.bottom) + "\nYahtzee: "+ str(self.yahtzee)
+
+    def __eq__(self, other):
+        if (self.top == other.get_top() and
+               self.top_score == other.get_top_score() and
+               self.bottom == other.get_bottom() and
+               self.yahtzee == other.get_yahtzee()):
+                  return True
+        return False
+    
+    def mark_top(self, number, scored):
+        new_card = copy.deepcopy(self)
+        new_card.update_top(number, scored)
+        return new_card
+    
+    def update_top(self, number, scored):
+        self.top[number-1] = True
+        self.top_score = self.top_score + scored
+        if self.top_score>63:
+            self. top_score = 63
+            
+    def mark_bottom(self, which, got_yahtzee=False):
+        new_card = copy.deepcopy(self)
+        new_card.update_bottom(which, got_yahtzee)
+        return new_card
+    
+    def update_bottom(self, which, got_yahtzee=False):
+        self.bottom[which] = True 
+        if which == 6:
+            if got_yahtzee:
+                self.yahtzee=True
+    
